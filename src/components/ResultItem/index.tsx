1d8e9d3e422cd8025033d535e82278337a1b23a1
xcland <xclancn@outlook.com>
2022-07-19 04:44:51 +0800

    feat: 添加 antd 和 tailwindcss 支持

--- src/components/ResultItem/index.tsx
+++ src/components/ResultItem/index.tsx
@@ -1,4 +1,5 @@
 import './index.css'
+import { Card } from 'antd'
 
 interface RIprops {
   type: string
@@ -7,16 +8,15 @@ interface RIprops {
   wam: string
 }
 
-const ResultItem: React.FC<RIprops> = (props) => {
+const ResultItem: React.FC<RIprops> = props => {
   // console.log(props);
   const { type, title, gpa, wam } = props
   return (
-    <div className={'resultCard'}>
-      <p className={'item-title'}>
-        {type}:{title}
-      </p>
-      <p className={'item-gpa'}>平均绩点: {gpa}</p>
-      <p className={'item-wam'}>加权平均分: {wam}</p>
+    <div className={'resultCard m-2'}>
+      <Card title={`${type}:${title}`}>
+        <p className={'item-gpa'}>平均绩点: {gpa}</p>
+        <p className={'item-wam'}>加权平均分: {wam}</p>
+      </Card>
     </div>
   )
 }
