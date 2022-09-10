5a42b87e8175102050d62779957e1aa21aff2b08
xcland <xclancn@outlook.com>
2022-09-11 01:20:43 +0800

    fix: fix style issue of new guide, remove old guide

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -8,44 +8,39 @@ import { ResultType } from '../../utils/Types'
 import ResultTabs from '../../components/ResultTabs'
 import { Button } from 'antd'
 import { useNavigate } from 'react-router-dom'
-import { open } from '@tauri-apps/api/shell'
+// import { open } from '@tauri-apps/api/shell'
 
 export const AppContext = React.createContext<ResultType | undefined>({
-    gpa: 0,
-    wam: 0,
-    bySemester: [],
-    byYear: []
+  gpa: 0,
+  wam: 0,
+  bySemester: [],
+  byYear: []
 })
 
-export function Home() {
-    const navigate = useNavigate();
+export function Home () {
+  const navigate = useNavigate()
 
-    const [gpaData, setGPAData] = useState<ResultType | undefined>(undefined)
-    // console.log(gpaData)
-    return (
-        <AppContext.Provider value={gpaData}>
-            <div>
-                <Header />
-                <div className={'readme'}>
-                    <Button
-                        type='link'
-                        onClick={async () => {
-                            try {
-                                await open('https://gpahelp.xcland.tech')
-                            } catch {
-                                window.open('https://gpahelp.xcland.tech', '_blank');
-                            }
-                        }}
-                    >
-                        使用指北
-                    </Button>
-                    <Button type='link' onClick={() => { navigate('guide') }}>使用指北（Beta）</Button>
-                </div>
-                <FileReader setGPAData={setGPAData} />
-                {/* <ResultList /> */}
-                {gpaData ? <ResultTabs /> : null}
-                {!gpaData ? <Footer /> : null}
-            </div>
-        </AppContext.Provider>
-    )
+  const [gpaData, setGPAData] = useState<ResultType | undefined>(undefined)
+  // console.log(gpaData)
+  return (
+    <AppContext.Provider value={gpaData}>
+      <div>
+        <Header />
+        <div className={'readme'}>
+          <Button
+            type='link'
+            onClick={() => {
+              navigate('guide')
+            }}
+          >
+            使用指北
+          </Button>
+        </div>
+        <FileReader setGPAData={setGPAData} />
+        {/* <ResultList /> */}
+        {gpaData ? <ResultTabs /> : null}
+        {!gpaData ? <Footer /> : null}
+      </div>
+    </AppContext.Provider>
+  )
 }
